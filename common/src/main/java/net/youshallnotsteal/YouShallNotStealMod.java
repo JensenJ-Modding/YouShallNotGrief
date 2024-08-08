03d5c6a2d1f3a08fc61b2df2de563062acdfe3c4
Jensen <jensenbradley@icloud.com>
2024-08-08 18:07:48 +0100

    Basic Event Registry
    Created a basic list of events we will want to track.

--- common/src/main/java/net/youshallnotsteal/YouShallNotStealMod.java
+++ common/src/main/java/net/youshallnotsteal/YouShallNotStealMod.java
@@ -1,9 +1,124 @@
 package net.youshallnotsteal;
 
+import dev.architectury.event.CompoundEventResult;
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.common.*;
+import dev.architectury.platform.Platform;
+import dev.architectury.utils.Env;
+import dev.architectury.utils.value.IntValue;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
 public class YouShallNotStealMod {
     public static final String MOD_ID = "youshallnotsteal";
     
     public static void init() {
+        registerEvents();
+    }
+
+    public static void registerEvents(){
+        //Only register events on serverside
+        if(Platform.getEnv() == Env.CLIENT.toPlatform())
+            return;
+
+        //Block Events
+        BlockEvent.BREAK.register((Level level, BlockPos pos, BlockState state, ServerPlayer player, @Nullable IntValue xp) -> {
+            System.out.println("Break block");
+            return EventResult.pass();
+        });
+
+        BlockEvent.PLACE.register((Level level, BlockPos pos, BlockState state, @Nullable Entity placer) -> {
+            System.out.println("Place block");
+            return EventResult.pass();
+        });
+
+        BlockEvent.FALLING_LAND.register((Level level, BlockPos pos, BlockState fallState, BlockState landOn, FallingBlockEntity entity) -> {
+            System.out.println("Falling block");
+        });
+
+        //Player events
+        PlayerEvent.FILL_BUCKET.register((Player player, Level level, ItemStack stack, @Nullable HitResult target) -> {
+            System.out.println("Fill bucket");
+            return CompoundEventResult.pass();
+        });
+
+        //Interaction events
+        InteractionEvent.FARMLAND_TRAMPLE.register((Level world, BlockPos pos, BlockState state, float distance, Entity entity) -> {
+            System.out.println("Trample");
+            return EventResult.pass();
+        });
+
+        InteractionEvent.INTERACT_ENTITY.register((Player player, Entity entity, InteractionHand hand) -> {
+            System.out.println("entity interact");
+            return EventResult.pass();
+        });
+
+        InteractionEvent.RIGHT_CLICK_ITEM.register((Player player, InteractionHand hand) -> {
+            System.out.println("click item on block");
+            return CompoundEventResult.pass();
+        });
+
+        InteractionEvent.RIGHT_CLICK_BLOCK.register((Player player, InteractionHand hand, BlockPos pos, Direction face) ->{
+            System.out.println("clicked block");
+            return EventResult.pass();
+        });
+
+        //Explosion Event
+        ExplosionEvent.DETONATE.register((Level level, Explosion explosion, List<Entity> affectedEntities) -> {
+            System.out.println("Explosion");
+        });
+
+        //Entity Events
+        EntityEvent.LIVING_DEATH.register((LivingEntity entity, DamageSource source) -> {
+            System.out.println("Entity death");
+            return EventResult.pass();
+        });
+
+        EntityEvent.LIVING_HURT.register((LivingEntity entity, DamageSource source, float amount) -> {
+            System.out.println("Entity hurt");
+            return EventResult.pass();
+        });
+
+        EntityEvent.ANIMAL_TAME.register((Animal animal, Player player) -> {
+            System.out.println("Entity tamed");
+            return EventResult.pass();
+        });
+
+        //Mixins
+        //  Fire
+        //  Adding and removing items from inventory
+        //  Non player break / place, maybe remove block events and just mixin directly
+
+        //EntityEvent
+        //  Entity hurt / death
+        //  Entity tame
+
+        //InteractionEvents
+        //  RightClickBlock
+        //    Door
+        //    Anything that has a menu, chest, furnace, enchant table etc.
+        //    Redstone components, button, lever, comparator, repeater
+        //    Sign Edit (1.20)
+        //  RightClickItem
+        //    Waxable Block (waxing and stripping)
+        //  InteractEntity
+        //    Item Frame
 
     }
 }
